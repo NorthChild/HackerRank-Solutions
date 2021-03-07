21e43a5ac1235fd7ee27e0ed476d627f956b0d18
Michael Snow Carini <65109289+NorthChild@users.noreply.github.com>
2021-03-07 12:02:05 +0100

    Update aVeryBigSum.py

--- Practice Exercises in Python/Algorithms/aVeryBigSum.py
+++ Practice Exercises in Python/Algorithms/aVeryBigSum.py
@@ -1,2 +1,30 @@
-def solve(meal_cost, tip_percent, tax_percent):
-    
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the aVeryBigSum function below.
+def aVeryBigSum(ar):
+    x = 0
+    y = 0
+    if (x < len(ar)):
+        for i in ar:
+            x += 1
+            y = y + i
+    return y
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
