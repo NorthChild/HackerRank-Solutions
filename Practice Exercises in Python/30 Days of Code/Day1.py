6aa33a5f8b7f489d08dad322da4cc3fbb2c8d808
Michael Snow Carini <65109289+NorthChild@users.noreply.github.com>
2021-03-07 11:59:32 +0100

    Update Day1.py

--- Practice Exercises in Python/30 Days of Code/Day1.py
+++ Practice Exercises in Python/30 Days of Code/Day1.py
@@ -1,2 +1,8 @@
-def solve(meal_cost, tip_percent, tax_percent):
-    
+int_var = int(input())
+float_var = float(input())
+str_var = str(input())
+
+
+print(i + int_var)
+print(d + float_var)
+print(s + str_var)
