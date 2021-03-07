291a55d1669704012fc329d3faa40586817fc1b7
Michael Snow Carini <65109289+NorthChild@users.noreply.github.com>
2021-03-07 12:07:33 +0100

    Update Python_If-Else.py

--- Practice Exercises in Python/Python/Python_If-Else.py
+++ Practice Exercises in Python/Python/Python_If-Else.py
@@ -1,2 +1,27 @@
-def solve(meal_cost, tip_percent, tax_percent):
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
     
+    checkedN = n % 2
+
+    if (checkedN == 0 and 2 <= n <= 5):
+        #print(checkedN)
+        print("Not Weird")
+    elif (checkedN == 0 and 6 <= n <= 20):
+        #print(checkedN)
+        print("Weird")
+    elif (checkedN == 0 and n > 20):
+        #print(checkedN)
+        print("Not Weird")
+    else:
+        #print(checkedN)
+        print("Weird")
