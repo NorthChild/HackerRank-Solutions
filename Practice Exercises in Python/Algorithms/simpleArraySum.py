1199bbc6e767dcd54bfc43ad47f218dfa244028f
Michael Snow Carini <65109289+NorthChild@users.noreply.github.com>
2021-03-07 12:01:13 +0100

    Update simpleArraySum.py

--- Practice Exercises in Python/Algorithms/simpleArraySum.py
+++ Practice Exercises in Python/Algorithms/simpleArraySum.py
@@ -1,2 +1,29 @@
-def solve(meal_cost, tip_percent, tax_percent):
-    
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the simpleArraySum function below.
+#   
+def simpleArraySum(ar):
+    x = 0
+    y = 0
+    if (x < len(ar)):
+        for i in ar:
+            x += 1
+            y = y + i
+    return y
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
