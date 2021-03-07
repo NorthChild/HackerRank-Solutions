ffc0db7072805d30b02d59c1619549f7cd074d2d
Michael Snow Carini <65109289+NorthChild@users.noreply.github.com>
2021-03-07 12:07:54 +0100

    Update ArithmeticOperators.py

--- Practice Exercises in Python/Python/ArithmeticOperators.py
+++ Practice Exercises in Python/Python/ArithmeticOperators.py
@@ -1,2 +1,11 @@
-def solve(meal_cost, tip_percent, tax_percent):
-    
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+sum_of_n = a + b
+difference = a - b
+product = a * b
+
+print(sum_of_n)
+print(difference)
+print(product)
