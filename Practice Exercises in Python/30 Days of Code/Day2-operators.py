a29a6e8840aeaeecf823fbacb76f39db808a7b36
Michael Snow Carini <65109289+NorthChild@users.noreply.github.com>
2021-03-07 16:04:59 +0100

    Add files via upload

---
+++ Practice Exercises in Python/30 Days of Code/Day2-operators.py
@@ -0,0 +1,9 @@
+
+
+def solve(meal_cost, tip_percent, tax_percent):
+    meal_after_tip = (meal_cost / 100) * tip_percent
+    meal_after_tax = (meal_cost / 100) * tax_percent
+
+    tax_plus_tip = (meal_after_tax + meal_after_tip)
+    total_cost = (meal_cost + tax_plus_tip)
+    print(round(total_cost))
