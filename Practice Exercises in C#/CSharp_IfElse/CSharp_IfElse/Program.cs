5e2f2b1ccf55945f3071b8439a5ba5ae3439f3ea
Michael Carini <michael.carini@asos.com>
2021-10-15 10:36:01 +0100

    added new folder for c# exercises

---
+++ Practice Exercises in C#/CSharp_IfElse/CSharp_IfElse/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CSharp_IfElse
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+
+
+    }
+}
---
+++ Practice Exercises in C#/CSharp_IfElse/TestExercise/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using CSharp_IfElse;
+
+namespace TestExercise
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
